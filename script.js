9da26252abd4261b9d5c7fbb3f5061f77fe3f841
leesovska <leesovska@gmail.com>
2024-04-16 10:41:19 +0300

    Домашнее задание № 9
    Исправлена ошибка: при нажатии на break кнопка становится активной.
    Также доработан код в части, когда таймер доходит до 00:00, он сбрасывается  в начальное состояние: 25:00/05:00.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const startButton = document.querySelector('#start');
 const resetButton = document.querySelector('#reset');
 const pomodoroButton = document.querySelector('#pomodoro');
 const breakButton = document.querySelector('#break');
+const buttons = document.querySelectorAll('.btn');
 
 let intervalId;
 let mode;
@@ -16,8 +17,10 @@ function startTimer() {
     if (seconds === 0) {
       if (minutes === 0) {
         stopTimer();
-        timerDisplay.textContent = '25:00';
-        startButton.textContent = 'start';
+        setTimeout(() => {
+          resetTimer();
+          startButton.textContent = 'start';
+        }, 500);
       } else {
         minutes--;
         seconds = 59;
@@ -32,14 +35,14 @@ function startTimer() {
     } else {
       formattedMinutes = '' + minutes;
     }
-  
+
     let formattedSeconds;
     if (seconds < 10) {
       formattedSeconds = '0' + seconds;
     } else {
       formattedSeconds = '' + seconds;
     }
-  
+
     timerDisplay.textContent = formattedMinutes + ':' + formattedSeconds;
   }, 0.5);
 }
@@ -54,6 +57,12 @@ function resetTimer() {
   } else {
     timerDisplay.textContent = '05:00';
   }
+}
+
+function removeActiveClass() {
+  buttons.forEach((button) => {
+    button.classList.remove('active');
+  });
 }
 
 startButton.addEventListener('click', () => {
@@ -77,6 +86,9 @@ pomodoroButton.addEventListener('click', () => {
   stopTimer();
   resetTimer();
   startButton.textContent = 'start';
+
+  removeActiveClass();
+  pomodoroButton.classList.add('active');
 });
 
 breakButton.addEventListener('click', () => {
@@ -84,4 +96,7 @@ breakButton.addEventListener('click', () => {
   stopTimer();
   resetTimer();
   startButton.textContent = 'start';
+
+  removeActiveClass();
+  breakButton.classList.add('active');
 });
